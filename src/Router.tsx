048b7a4cf8569ab37d816b8b795bfde8e20ca727
filipesaretta <filipessm124@gmail.com>
2023-02-24 12:25:38 -0300

    Add new route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Reservations } from './pages/Reservations';
+import { Success } from './pages/Success';
 
 export function Router() {
   return (
@@ -8,7 +9,7 @@ export function Router() {
       <Route path="/">
         <Route path="/" element={<Home />} />
         <Route path="/reservations" element={<Reservations />} />
-        {/* <Route path="/success" element={<Success />} /> */}
+        <Route path="/success" element={<Success />} />
       </Route>
     </Routes>
   );
