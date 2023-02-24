68d10aec5b44cf368ae2cb94514b0f6dcd805716
filipesaretta <filipessm124@gmail.com>
2023-02-24 00:31:50 -0300

    Create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { AddNewPatient } from '../../components/AddNewPatient';
+import { Header } from '../../components/Header';
+
+export function Home() {
+  return (
+    <div>
+      <Header />
+      <AddNewPatient />
+    </div>
+  );
+}
