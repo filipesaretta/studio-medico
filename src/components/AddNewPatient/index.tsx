80cd0a6696fd3310520a677fa46c95a661b97803
filipesaretta <filipessm124@gmail.com>
2023-02-24 12:25:51 -0300

    Create add new patient

--- src/components/AddNewPatient/index.tsx
+++ src/components/AddNewPatient/index.tsx
@@ -1,11 +1,61 @@
-import { useForm } from 'react-hook-form';
-import { FormContainer, Form } from './styles';
+import { Controller, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+import DatePicker from 'react-datepicker';
+import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../lib/axios';
+import { dateFormatter } from '../../utils/formatter';
+
+import { FormContainer, Form, Button } from './styles';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const newPatientSchemaValidation = zod
+  .object({
+    name: zod.string().min(2, 'Aggiungi name'),
+    codiceFiscale: zod
+      .string()
+      .length(16, 'Codice Fiscale deve avere 16 caratteri'),
+    dateInput: zod.date({
+      required_error: 'Seleziona una data',
+      invalid_type_error: 'Questo non è una data',
+    }),
+  })
+  .required();
+
+type newPatientData = zod.infer<typeof newPatientSchemaValidation>;
 
 export function AddNewPatient() {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<newPatientData>({
+    resolver: zodResolver(newPatientSchemaValidation),
+  });
+  const navigate = useNavigate();
 
-  function handleAddNewPatient(data) {
-    console.log(data);
+  async function handleAddNewPatient(data: newPatientData) {
+    const ticket = uuidv4();
+    const patientActive = true;
+    const { dateInput, codiceFiscale, name } = data;
+
+    await api.post('/reservations', {
+      id: ticket,
+      createdAt: dateFormatter.format(new Date(dateInput)),
+      codiceFiscale,
+      name,
+      prenotazioneAttiva: patientActive,
+    });
+    navigate('/success');
+  }
+  /**
+   * This function checks if it's weekend. Only can select week days.
+   */
+  function isWeekday(date: Date) {
+    const day = date.getDay();
+    return day !== 0 && day !== 6;
   }
 
   return (
@@ -13,15 +63,33 @@ export function AddNewPatient() {
       <Form onSubmit={handleSubmit(handleAddNewPatient)}>
         <label htmlFor="name">Nome</label>
         <input type="text" placeholder="Nome" id="name" {...register('name')} />
+        <span>{errors.name?.message}</span>
 
         <label htmlFor="">Codice Fiscale</label>
         <input
           type="text"
           placeholder="Codice Fiscale"
-          {...register('codice-fiscale')}
+          {...register('codiceFiscale')}
         />
+        <span>{errors.codiceFiscale?.message}</span>
 
-        <button type="submit">Aggiungi Paziente</button>
+        <div>
+          <Controller
+            control={control}
+            name="dateInput"
+            render={({ field }) => (
+              <DatePicker
+                onChange={(date) => field.onChange(date)}
+                filterDate={isWeekday}
+                selected={field.value}
+                inline
+              />
+            )}
+          />
+        </div>
+        <span>{errors.dateInput?.message}</span>
+
+        <Button type="submit">Aggiungi Paziente</Button>
       </Form>
     </FormContainer>
   );
