b0ba82d56e29abc0dec283016927921528ef508c
filipesaretta <filipessm124@gmail.com>
2023-02-24 12:26:04 -0300

    Add comment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import { ReservationsLink, HeaderContainer, HeaderContent } from './styles';
 
@@ -8,10 +8,12 @@ export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logo} alt="Draw of a doctor" />
-
+        <Link to="/">
+          <img src={logo} alt="Draw of a doctor" />
+        </Link>
+        {/* This is not the correct way handle, I've done this way because I was not able to figured out how to make a rerender when I added new Patient. Here I'm forcing the reloading. */}
         <ReservationsLink
-          to={pathname === '/reservations' ? '/' : '/reservations'}
+          href={pathname === '/reservations' ? '/' : '/reservations'}
         >
           {pathname === '/reservations' ? 'Aggiungi Paziente' : 'Reservations'}
         </ReservationsLink>
