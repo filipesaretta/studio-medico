d5169dc3f45e969426fb4d9aaafb52c85b91e83b
filipesaretta <filipessm124@gmail.com>
2023-02-24 00:31:18 -0300

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { ThemeProvider } from 'styled-components';
+import { BookCalendar } from './components/BookCalendar';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/globals';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 function App() {
-
   return (
-    <div className="App">
-     <h1>Book A doctor</h1>
-    </div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+        {/* <BookCalendar/> */}
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Reservations } from './pages/Reservations';
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route path="/" element={<Home />} />
+        <Route path="/reservations" element={<Reservations />} />
+        {/* <Route path="/success" element={<Success />} /> */}
+      </Route>
+    </Routes>
+  );
+}
